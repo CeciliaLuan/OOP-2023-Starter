894f222dc8890e946a0ffe515b207aba2f7e7560
C21379843 Cecilia Luan <c21379843@mytudublin.ie>
2023-04-26 17:43:27 +0100

    bla

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -6,63 +6,40 @@ import processing.core.PApplet;
 
 public class DANI extends PApplet {
 
-	
+
 
 	public void settings() {
 		size(1000, 1000);
-		//fullScreen(SPAN);
+		// fullScreen(SPAN);
 	}
 
-    String[] sonnet;
+	String[] sonnet;
+	private String w;
 
-    public String[] writeSonnet()
-    {
-        return null;
-    }
+	public String[] writeSonnet() {
+		return null;
+	}
 
 	public void loadfile() {
-		String[] sonnet = loadStrings("sonnet.txt");
-		for (String s : sonnet) {
-			System.out.println(s);
-		}
-	}
+		String[] sonnet = loadStrings("small.txt");
+		for(String s: sonnet)
+		{
+		String[] words = s.split(" "); // Split a string into an array of words
+        w.replaceAll("[^\\w\\s]",""); // Remove punction characters
+        s.toLowerCase(); // Convert a string to lower case 
+		}}
 
-
-	public void findWord(String str) {
-		for (String s : sonnet) {
-			if (s.contains(str)) {
-				System.out.println(s);
-			}
-		}
-	}
-
-
-	//loads the small.txt file
-
-	public void loadfile() {
-		String[] small = loadStrings("small.txt");
-		for (String s : small) {
-			System.out.println(s);
-		}
-	}
-
-//prints the small.txt file
+	// prints the small.txt file
 	public void printModel() {
 		String[] small = loadStrings("small.txt");
 		for (String s : small) {
 			System.out.println(s);
 		}
 	}
-	
-	public void writeSonnet()
-	{
-		
-	}
 
 	public void setup() {
 		colorMode(HSB);
 
-       
 	}
 
 	public void keyPressed() {
@@ -71,13 +48,71 @@ public class DANI extends PApplet {
 
 	float off = 0;
 
-	public void draw() 
-    {
+	public void draw() {
 		background(0);
 		fill(255);
 		noStroke();
 		textSize(20);
-        textAlign(CENTER, CENTER);
-        
+		textAlign(CENTER, CENTER);
+
+	}
+
+	public class Follow {
+		private String word;
+		private int count;
+	
+		public Follow(String word, int count) {
+			this.word = word;
+			this.count = count;
+		}
+	
+		public String getWord() {
+			return word;
+		}
+	
+		public int getCount() {
+			return count;
+		}
+	
+		public String toString() {
+			return word + ": " + count;
+		}
+	}
+	
+	public class Word {
+		private String word;
+		private ArrayList<Follow> follows;
+	
+		public Word(String word) {
+			this.word = word;
+			this.follows = new ArrayList<Follow>();
+		}
+	
+		public String getWord() {
+			return word;
+		}
+	
+		public ArrayList<Follow> getFollows() {
+			return follows;
+		}
+	
+		public void addFollow(String word, int count) {
+			follows.add(new Follow(word, count));
+		}
+	
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			sb.append(word).append(": ");
+			for (Follow follow : follows) {
+				sb.append(follow.toString()).append(", ");
+			}
+			sb.delete(sb.length() - 2, sb.length());
+			return sb.toString();
+		}
 	}
 }
+
+
+
+
+
